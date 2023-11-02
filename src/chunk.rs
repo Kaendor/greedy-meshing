fa32835a7dd2c9c6ab919ed47dda41fbb59f336c
Kaendor <dimitri.teboul@viaco.fr>
2023-11-02 22:42:21 +0100

    render chunk naively

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,4 +1,8 @@
-use crate::CHUNK_SIZE_CUBED;
+use crate::{mesh::create_cube_vertices_at, CHUNK_SIZE, CHUNK_SIZE_CUBED, CHUNK_SIZE_SQUARED};
+use bevy::{
+    prelude::*,
+    render::{mesh::Indices, render_resource::PrimitiveTopology},
+};
 
 pub struct Chunk {
     pub voxels: Vec<Voxel>,
@@ -12,6 +16,41 @@ impl Chunk {
             })
             .collect();
         Chunk { voxels }
+    }
+
+    pub fn generate_mesh(&self) -> Mesh {
+        let mut cube_mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        let mut positions = Vec::new();
+        let mut normals = Vec::new();
+        let mut indices = Vec::new();
+
+        for (i, v) in self.voxels.iter().enumerate() {
+            let z = i / (CHUNK_SIZE_SQUARED);
+
+            let ti = i - (z * CHUNK_SIZE_SQUARED);
+
+            let x = ti % CHUNK_SIZE;
+            let y = ti / CHUNK_SIZE;
+            let index = UVec3::new(x as u32, y as u32, z as u32);
+
+            let (pos, n, id) = create_cube_vertices_at(&index);
+
+            positions.extend(pos);
+            normals.extend(n);
+            indices.extend(id.into_iter().map(|n| n as u32 + positions.len() as u32));
+        }
+
+        cube_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+
+        // For meshes with flat shading, normals are orthogonal (pointing out) from the direction of
+        // the surface.
+        // Normals are required for correct lighting calculations.
+        // Each array represents a normalized vector, which length should be equal to 1.0.
+        cube_mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+
+        cube_mesh.set_indices(Some(Indices::U32(indices)));
+
+        cube_mesh
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ use bevy_panorbit_camera::PanOrbitCameraPlugin;
 use chunk::Chunk;
 use diagnostics::MeshDiagnosticPlugin;
 use inspector::DiagnosticInspectorPlugin;
-use mesh::create_cube_mesh;
 
 pub const CHUNK_SIZE: usize = 3;
 pub const CHUNK_SIZE_SQUARED: usize = CHUNK_SIZE * CHUNK_SIZE;
@@ -49,32 +48,23 @@ fn setup(
 ) {
     // Import the custom texture.
     // Create and save a handle to the mesh.
-    let cube_mesh_handle: Handle<Mesh> = meshes.add(create_cube_mesh());
 
     let chunk = Chunk::new();
+    let chunk_mesh = chunk.generate_mesh();
+    let chunk_mesh_handle: Handle<Mesh> = meshes.add(chunk_mesh);
 
-    for (i, v) in chunk.voxels.iter().enumerate() {
-        let z = i / (CHUNK_SIZE_SQUARED);
-
-        let ti = i - (z * CHUNK_SIZE_SQUARED);
-
-        let x = ti % CHUNK_SIZE;
-        let y = ti / CHUNK_SIZE;
-        let index = UVec3::new(x as u32, y as u32, z as u32);
-        commands.spawn((
-            PbrBundle {
-                mesh: cube_mesh_handle.clone(),
-                material: materials.add(StandardMaterial {
-                    base_color: Color::rgba(0.2, 0.7, 0.1, 0.1),
-                    alpha_mode: AlphaMode::Mask(0.5),
-                    ..default()
-                }),
-                transform: Transform::from_xyz(index.x as f32, index.y as f32, index.z as f32),
+    commands.spawn((
+        PbrBundle {
+            mesh: chunk_mesh_handle.clone(),
+            material: materials.add(StandardMaterial {
+                base_color: Color::rgba(0.2, 0.7, 0.1, 0.1),
+                alpha_mode: AlphaMode::Mask(0.5),
                 ..default()
-            },
-            Wireframe,
-        ));
-    }
+            }),
+            ..default()
+        },
+        Wireframe,
+    ));
 
     // Transform for the camera and lighting, looking at (0,0,0) (the position of the mesh).
     let camera_and_light_transform =
@@ -105,6 +95,4 @@ fn setup(
         transform: camera_and_light_transform,
         ..default()
     });
-
-    // Text to describe the controls.
 }
