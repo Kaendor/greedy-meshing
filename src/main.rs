90c160f65a593f59391f5d5edf559f1c54e5657d
Kaendor <dimitri.teboul@viaco.fr>
2023-11-02 21:37:28 +0100

    compute indices based on face

--- src/main.rs
+++ src/main.rs
@@ -162,6 +162,24 @@ impl FaceDirection {
         }
     }
 
+    // 0,3,1 , 1,3,2, // triangles making up the top (+y) facing side.
+    // 4,5,7 , 5,6,7, // bottom (-y)
+    // 8,11,9 , 9,11,10, // right (+x)
+    // 12,13,15 , 13,14,15, // left (-x)
+    // 16,19,17 , 17,19,18, // back (+z)
+    // 20,21,23 , 21,22,23, // forward (-z)
+    fn indices(&self, i: usize) -> [usize; 6] {
+        let i = i * 4;
+        match self {
+            FaceDirection::Top | FaceDirection::Right | FaceDirection::Back => {
+                [i, i + 3, i + 1, i + 1, i + 3, i + 2]
+            }
+            FaceDirection::Bottom | FaceDirection::Left | FaceDirection::Forward => {
+                [i, i + 1, i + 3, i + 1, i + 2, i + 3]
+            }
+        }
+    }
+
     fn normals(&self) -> [[f32; 3]; 4] {
         match self {
             FaceDirection::Top => [
@@ -204,22 +222,34 @@ impl FaceDirection {
     }
 }
 
-fn create_cube_vertices_at(pos: &IVec3) -> (Vec<[f32; 3]>, Vec<[f32; 3]>) {
+fn create_cube_vertices_at(pos: &IVec3) -> (Vec<[f32; 3]>, Vec<[f32; 3]>, Vec<usize>) {
     // Each array is an [x, y, z] coordinate in local space.
     // Meshes always rotate around their local [0, 0, 0] when a rotation is applied to their Transform.
     // By centering our mesh around the origin, rotating the mesh preserves its center of mass.
-    FaceDirection::iter()
+    // f.vertices().into_iter().zip(f.normals().into_iter())
+    let (pos, n) = FaceDirection::iter()
         // for each face, associate normals
         .flat_map(|f| f.vertices().into_iter().zip(f.normals().into_iter()))
         // add the required position to each vertex
         .map(|(v, n)| (Vec3::from_array(v) + pos.as_vec3(), n))
         .map(|(v, n)| (v.to_array(), n))
-        .unzip()
+        .unzip();
+
+    let indices = FaceDirection::iter()
+        .enumerate()
+        .flat_map(|(i, f)| f.indices(i))
+        .collect();
+
+    (pos, n, indices)
 }
 
 fn create_cube_mesh() -> Mesh {
     let mut cube_mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    let (vertices, normals) = create_cube_vertices_at(&IVec3::ZERO);
+    let (vertices, normals, indices) = create_cube_vertices_at(&IVec3::ZERO);
+
+    info!("vertices: {:?}", vertices);
+    info!("normals: {:?}", normals);
+    info!("indices: {:?}", indices);
 
     #[rustfmt::skip]
     cube_mesh.insert_attribute(
@@ -267,15 +297,16 @@ normals
     // should appear counter-clockwise from the front of the triangle, in this case from outside the cube).
     // Read more about how to correctly build a mesh manually in the Bevy documentation of a Mesh,
     // further examples and the implementation of the built-in shapes.
-    #[rustfmt::skip]
-    cube_mesh.set_indices(Some(Indices::U32(vec![
-        0,3,1 , 1,3,2, // triangles making up the top (+y) facing side.
-        4,5,7 , 5,6,7, // bottom (-y) 
-        8,11,9 , 9,11,10, // right (+x)
-        12,13,15 , 13,14,15, // left (-x)
-        16,19,17 , 17,19,18, // back (+z)
-        20,21,23 , 21,22,23, // forward (-z)
-    ])));
+    // vec![
+    //         0,3,1 , 1,3,2, // triangles making up the top (+y) facing side.
+    //         4,5,7 , 5,6,7, // bottom (-y)
+    //         8,11,9 , 9,11,10, // right (+x)
+    //         12,13,15 , 13,14,15, // left (-x)
+    //         16,19,17 , 17,19,18, // back (+z)
+    //         20,21,23 , 21,22,23, // forward (-z)
+    //     ]
+    let indices = indices.into_iter().map(|u| u as u32).collect();
+    cube_mesh.set_indices(Some(Indices::U32(indices)));
 
     cube_mesh
 }
